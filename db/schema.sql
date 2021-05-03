7a86934be0798b18a19fa6a966b6eda57a579563
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-03 19:37:30 -0400

    finished writing schema.sql code

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,24 @@ CREATE DATABASE employee_DB;
 
 USE employee_DB;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL to hold role salary,
+    department_id INT, 
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    PRIMARY KEY (id)
+)
