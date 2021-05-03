5cb355787e29f16f5635b862b3bc1ee29de0e534
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-03 19:51:06 -0400

    added appropriate syntax to schema file since it was not working

--- db/schema.sql
+++ db/schema.sql
@@ -6,22 +6,22 @@ USE employee_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30),
+    name VARCHAR(30) NULL,
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL to hold role salary,
+    title VARCHAR(30)NULL,
+    salary DECIMAL (10,4),
     department_id INT, 
     PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
     role_id INT,
     PRIMARY KEY (id)
-)
+);
