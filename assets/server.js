29443ca36244efb8db78f803caa51df98fad66ee
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-03 21:43:24 -0400

    started working on the inquirer prompt and .then function in server.js file

--- assets/server.js
+++ assets/server.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 
   password: "",
 
-  database: "employee",
+  database: "employee_DB",
 });
 
 connection.connect((err) => {
@@ -18,5 +18,50 @@ connection.connect((err) => {
 });
 
 const runSearch = () => {
-  inquirer.prompt({});
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    })
+
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all employees":
+          employeeSearch();
+          break;
+
+        case "View all employees by department":
+          departmentSearch();
+          break;
+
+        case "View all employees by manager":
+          managerSearch();
+          break;
+
+        case "Update employee":
+          updateEmployeeSearch();
+          break;
+
+        case "Remove Employee":
+          removeEmployeeSearch();
+          break;
+
+        case "Update Employee Role":
+          roleSearch();
+          break;
+
+        case "Update Employee Manager":
+          updateManagerSearch();
+          break;
+      }
+    });
 };
