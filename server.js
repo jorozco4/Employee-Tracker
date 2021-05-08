e725b72aff6de7766b35d0f744d83656b7ad5fef
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-07 22:28:04 -0400

    updated EmpRol and addEmployee

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+const chalk = require("chalk");
+const figlet = require("figlet");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -17,6 +19,7 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId + "\n");
   runSearch();
 });
+
 //List questions to answer
 const runSearch = () => {
   inquirer
@@ -97,52 +100,74 @@ const viewAllEmpByRole = () => {
 };
 //Adds Employees
 const addEmployee = () => {
-  console.log("Inserting a new employee.\n");
+  // Solution so far to be able to reference an index for when choosing an employee role
+  let roles = [
+    "Accountant",
+    "Lead Engineer",
+    "Assistant Director",
+    "Sales Lead",
+    "Lawyer",
+  ];
+
   inquirer
     .prompt([
       {
-        type: "input",
-        message: "First Name?",
-        name: "first_name",
-      },
-      {
-        type: "input",
-        message: "Last Name?",
-        name: "last_name",
-      },
-      {
+        message: "What would be the employee's role?",
         type: "list",
-        message: "What is the employee's role?",
-        name: "role_id",
-        choices: [1, 2, 3],
+        name: "role",
+        choices: roles,
+      },
+      {
+        message: "What is the employees first name?",
+        type: "input",
+        name: "firstName",
+      },
+      {
+        message: "What is the employees last name?",
+        type: "input",
+        name: "lastName",
       },
     ])
-    .then(function (res) {
-      const employee = res.employee;
-      const query = `INSERT INTO department (name) VALUES("${employee}")`;
-      connection.query(query, function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        runSearch();
-      });
+    .then((answers) => {
+      connection.query(
+        `
+           INSERT INTO employee SET ?`,
+        {
+          first_name: answers.firstName,
+          last_name: answers.lastName,
+          role_id: roles.indexOf(answers.role) + 1, // This line has to be an INT
+        },
+
+        function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          runSearch();
+        }
+      );
     });
 };
 //Adds Departments
 const addDepartment = () => {
   inquirer
-    .prompt({
-      type: "input",
-      message: "What is the name of the department you want to add?",
-      name: "department",
-    })
-    .then(function (res) {
-      const department = res.department;
-      const query = `INSERT INTO department (name) VALUES("${department}")`;
-      connection.query(query, function (err, res) {
-        if (err) throw err;
-        console.table(res);
-        runSearch();
-      });
+    .prompt([
+      {
+        message: "What is the name of the Department?",
+        type: "input",
+        name: "newDept",
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        `
+           INSERT INTO department SET ?`,
+        {
+          name: answers.newDept,
+        },
+        (err, result) => {
+          if (err) throw err;
+        }
+      );
+      runSearch();
     });
 };
 //Adds Employee Roles
@@ -178,29 +203,45 @@ const addRole = () => {
 };
 //Updates Employee Role
 const updateEmpRole = () => {
+  let employees = [
+    "John Doe",
+    "Mike Chan",
+    "Ashley Rodgriuez",
+    "Kevin Tupik",
+    "Sarah Lourd",
+  ];
+
+  let roles = [
+    "Accountant",
+    "Lead Engineer",
+    "Assistant Director",
+    "Sales Lead",
+    "Lawyer",
+  ];
+
   inquirer
     .prompt([
       {
-        type: "input",
-        message: "Which employee would you like to update?",
-        name: "employeeUpdate",
+        message: "Which Employee do you wish to update?",
+        type: "list",
+        name: "employee",
+        choices: employees,
       },
-
       {
-        type: "input",
-        message: "What do you want to update to?",
-        name: "updateRole",
+        message: "What is his/her new role?",
+        type: "list",
+        name: "role",
+        choices: roles,
       },
     ])
-    .then(function (answer) {
+    .then(function (response) {
       connection.query(
-        "UPDATE employee SET role_id=? WHERE first_name= ?",
-        [answer.updateRole, answer.employeeUpdate],
-        function (err, res) {
-          if (err) throw err;
-          console.table(res);
-          runSearch();
+        "INSERT INTO employee (role_id) values (?, ?, ?)",
+        [response.title, response.salary, response.department_id],
+        function (err, data) {
+          console.table(data);
         }
       );
+      runSearch();
     });
 };
